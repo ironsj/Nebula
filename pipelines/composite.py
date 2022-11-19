86b9474f998efef63d7cc10cd6fe6808c0933cf7
ironsj <jakeirons007@gmail.com>
2022-11-19 00:37:31 -0500

    Updating other pipeline files

--- pipelines/composite.py
+++ pipelines/composite.py
@@ -1,13 +1,25 @@
-import nebula.connector
+import os
+import sys
+
+# Most recent version of Python3 needs some help to find reletive paths to modules.
+# The follwoing three lines will ensure that Python3 will be able to find the modules
+# that are needed in this file.
+script_dir = os.path.dirname( __file__ )
+nebula_pipeline_dir = os.path.join( script_dir, '..', 'Nebula-Pipeline')
+sys.path.append(nebula_pipeline_dir)
+data_controller_dir = os.path.join( script_dir, '..', 'Nebula-Pipeline', 'data_controller')
+sys.path.append(data_controller_dir)
+model_dir = os.path.join( script_dir, '..', 'Nebula-Pipeline', 'model')
+sys.path.append(model_dir)
+
+from nebula.pipeline import Pipeline
+from nebula.connector import SocketIOConnector
 from nebula.data_controller.CSVDataController import CSVDataController
 from nebula.model.ActiveSetModel import ActiveSetModel
 from nebula.model.CompositeModel import CompositeModel
-import nebula.pipeline
+import asyncio
 
-import sys
-import zerorpc
-
-def main():
+async def main():
     if len(sys.argv) < 4:
         print("Usage: python main.py <port> <csv file path> <raw data folder path> <pipeline arguments>")
     
@@ -15,7 +27,7 @@ def main():
     print(csvfile)
     raw_folder = sys.argv[3]
     
-    pipeline = nebula.pipeline.Pipeline()
+    pipeline = Pipeline()
    
     relevance = ActiveSetModel()
     composite = CompositeModel()
@@ -25,10 +37,12 @@ def main():
     pipeline.append_model(composite)
     pipeline.set_data_controller(data_controller)
     
-    connector = nebula.connector.ZeroMQConnector(port=int(sys.argv[1]))
+    connector = SocketIOConnector(port=int(sys.argv[1]))
     pipeline.set_connector(connector)
+    
+    await connector.makeConnection(port=int(sys.argv[1]))
     
     pipeline.start(sys.argv[4:])
     
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
